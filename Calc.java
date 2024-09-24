da65193d26c3497164eff8464d86eafe6ad851a4
Mainak Mukherjee <mainakcr72002@gmail.com>
2024-09-24 17:23:44 +0530

    Input expression format changed, exception handling added for ZeroDivisonException

--- Calc.java
+++ Calc.java
@@ -1,52 +1,60 @@
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Calc {
-    public static int add(int a, int b){
+    public static int add(int a, int b) {
         return a + b;
     }
 
-    public static int sub(int a, int b){
+    public static int sub(int a, int b) {
         return a - b;
     }
 
-    public static int mul(int a, int b){
+    public static int mul(int a, int b) {
         return a * b;
     }
 
-    public static double div(int a, int b){
-        return (double) a / b;
+    public static double div(int a, int b) throws Exception {
+        return a / b;
     }
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Enter the expression : ");
+
+        System.out.println("--- Enter the expression --- ");
+        System.out.println("Format should be like:  <operand> <operator> <operand>");
+        System.out.println("For example: 10 + 2");
+
+        System.out.print("Enter the expression: ");
         String exp = sc.nextLine();
+        String parsedArr[] = exp.split(" ");
+        int a = Integer.parseInt(String.valueOf(parsedArr[0]));
+        int b = Integer.parseInt(String.valueOf(parsedArr[2]));
+        char op = parsedArr[1].charAt(0);
 
-        int a = Integer.parseInt(String.valueOf(exp.charAt(0)));
-        int b = Integer.parseInt(String.valueOf(exp.charAt(2)));
-        char c = exp.charAt(1);
-
-        switch (c) {
+        switch (op) {
             case '+':
-                System.out.println(a + " + " + b  + " = " + Calc.add(a, b));
+                System.out.println(a + " + " + b + " = " + Calc.add(a, b));
                 break;
 
             case '-':
-                System.out.println(a + " - " + b  + " = " + Calc.sub(a, b));
+                System.out.println(a + " - " + b + " = " + Calc.sub(a, b));
                 break;
 
             case '*':
-                System.out.println(a + " * " + b  + " = " + Calc.mul(a, b));
+                System.out.println(a + " * " + b + " = " + Calc.mul(a, b));
                 break;
 
             case '/':
-                double value =Double.parseDouble(new DecimalFormat("##.###").format(Calc.div(a, b)));
-                System.out.println(a + " / " + b  + " = " + value);
+                try {
+                    double value = Calc.div(a, b);
+                    System.out.println(a + " / " + b + " = " + value);
+                } catch (Exception e) {
+                    System.out.println(e);
+                    System.out.println(e.getMessage());
+                }
+
                 break;
-        
+
             default:
                 System.out.println("Wrong input...");
                 break;
@@ -55,5 +63,3 @@ public class Calc {
         sc.close();
     }
 }
-
-
